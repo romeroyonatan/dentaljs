b73758babcb4d435118afed5778fd130f07952fa
Yonatan Romero <yromero@openmailbox.org>
2016-03-27 22:47:46 -0300

    Fab file para automatizar tareas de mantenimiento

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+import datetime
+from fabric.api import local
+
+def deploy():
+    local('git pull origin master')
+    local('docker-compose build')
+    local('docker-compose up -d')
+
+def backup(output_dir=""):
+    ip = local("docker inspect --format '{{ .NetworkSettings.Networks.dentaljs_default.IPAddress }}' dentaljs_db_1",
+               capture=True)
+    if not output_dir:
+        output_dir = datetime.date.today().strftime('backup/%Y%m%d')
+
+    local("mkdir -p %s" % output_dir)
+    local("mongodump -o %s -h %s" % (output_dir, ip))
