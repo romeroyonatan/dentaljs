f0509e00497f9ac5d316b340b1fd2c06bce8d07a
Yonatan Romero <yromero@openmailbox.org>
2016-04-20 20:13:53 -0300

    FIX  tests

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config){
 
     files : [
       'bower_components/jquery/dist/jquery.js',
+      'bower_components/toastr/toastr.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-resource/angular-resource.js',
